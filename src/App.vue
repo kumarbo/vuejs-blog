c65ef6b77ea7fe0c3ce998f1e63b97df62787333
kumarbo <boharakumar12345@gmail.com>
2020-05-07 14:48:49 +0545

    http properties

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-    <add-blog></add-blog>
+    <show-blog></show-blog>
+    
 </template>
 
 <script>
 import addblog from './components/addblog.vue';
-
+import showblog from './components/showblog.vue';
 export default {
     components:{
-        'add-blog':addblog
+        'add-blog':addblog,
+        'show-blog':showblog
     },
  
   data () {
---
+++ src/components/showblog.vue
@@ -0,0 +1,41 @@
+<template>
+<div id="show-blog">
+    <h1>all blog article</h1>
+  <div v-for="blog in blogs" class="single-blog" v-bind:key="blog.id">
+      <h2>{{blog.title}}</h2>
+      <article>{{blog.body}}</article>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  
+  data () {
+    return {
+      blogs:[]
+    }
+  },
+  methods:{
+
+  },
+  created(){
+      this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+          this.blogs=data.body.slice(0,10);
+      })
+  }
+}
+</script>
+
+<style scoped>
+#show-blog{
+    max-width:800px;
+    margin:0 auto;
+}
+.single-blog{
+    padding:20px;
+    margin:20px 0;
+  box-sizing:border-box;
+    background:whitesmoke;
+}
+</style>
