63d57ffcb9b294722874fbe0fee7e0e457ed826a
kumarbo <boharakumar12345@gmail.com>
2020-05-08 20:55:41 +0545

    adding route links new head.vue is created

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+  <app-header></app-header>
   <router-view></router-view>
 </div>
 </template>
@@ -8,11 +9,13 @@
 import addblog from './components/addblog.vue';
 import showblog from './components/showblog.vue';
 import listblog from './components/showblog.vue';
+import head from './components/head.vue';
 export default {
     components:{
         'add-blog':addblog,
         'show-blog':showblog,
-        'list-blogs':listblog
+        'list-blogs':listblog,
+        'app-header':head
     },
  
   data () {
