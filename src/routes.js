258679c2b20e92abe1d84839f37ea2476fe13301
kumarbo <boharakumar12345@gmail.com>
2020-05-08 15:11:56 +0545

    route

---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import showblog from './components/showblog.vue';
+import addblog from './components/addblog.vue';
+export default[
+    {path:'/',component:showblog},
+    {path:'/add',component:addblog}
+]
